b182e80a6515f61bfcf1e036948e453a2fa76982
なるみ <weaper@gmail.com>
2018-05-30 17:24:54 +0800

    add num workers arg

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
     parser.add_argument('--root', type=str, default='data')
     parser.add_argument('--valid-ratio', type=float, default=0.2)
     parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--num-workers', type=int, default=0)
     config = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -24,7 +25,8 @@ def main():
     optimizer = optim.Adam(net.parameters(), lr=config.lr)
 
     train_loader, valid_loader = get_dataloader(
-        config.size, config.root, config.batch_size, config.valid_ratio)
+        config.size, config.root, config.batch_size, config.valid_ratio,
+        config.num_workers)
 
     trainer = Trainer(net, optimizer, train_loader, valid_loader, device)
     for epoch in range(config.epochs):
