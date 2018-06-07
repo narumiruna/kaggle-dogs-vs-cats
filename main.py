b711b819fee59d4f90d707e85e7c405b21166b8c
なるみ <weaper@gmail.com>
2018-06-07 17:43:24 +0800

    transfer

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import argparse
 
+import torch
 from torch import nn, optim
+from torchvision import models
 
+from dataloader import get_dataloader
 from model import Net
 from trainer import Trainer
-from dataloader import get_dataloader
-import torch
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--size', type=int, default=64)
+    parser.add_argument('--size', type=int, default=224)
     parser.add_argument('--root', type=str, default='data')
     parser.add_argument('--valid-ratio', type=float, default=0.2)
     parser.add_argument('--epochs', type=int, default=20)
@@ -21,8 +22,13 @@ def main():
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    net = Net(size=config.size, num_repeats=(1, 2, 3)).to(device)
-    optimizer = optim.Adam(net.parameters(), lr=config.lr)
+    net = models.resnet50(pretrained=True)
+    net.fc = nn.Linear(net.fc.in_features, 2)
+    net = net.to(device)
+    optimizer = optim.Adam(net.fc.parameters(), lr=config.lr)
+
+    # net = Net(size=config.size, num_repeats=(1, 2, 3)).to(device)
+    # optimizer = optim.Adam(net.parameters(), lr=config.lr)
 
     train_loader, valid_loader = get_dataloader(
         config.size, config.root, config.batch_size, config.valid_ratio,
